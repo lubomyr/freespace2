3473b6f3bdde9092f7850003eccab4df164f3288
Lyubomyr Lisen <lubomyr31@gmail.com>
2016-05-01 16:28:36 +0300

    multiarch support

--- AndroidBuild.sh
+++ AndroidBuild.sh
@@ -5,9 +5,8 @@ LOCAL_PATH=`cd $LOCAL_PATH && pwd`
 
 JOBS=1
 
-ln -sf libglshim.a $LOCAL_PATH/../../../obj/local/armeabi-v7a/libGL.a
+ln -sf libglshim.a $LOCAL_PATH/../../../obj/local/$1/libGL.a
 
-if [ "$1" = armeabi-v7a ]; then
 env CFLAGS="-include ../../android_debug.h" \
-../setEnvironment-armeabi-v7a.sh sh -c "cd freespace2 && make -j$JOBS" && mv -f freespace2/freespace2 libapplication-armeabi-v7a.so
-fi
+../setEnvironment-$1.sh sh -c "cd freespace2 && make -j$JOBS" && mv -f freespace2/freespace2 libapplication-$1.so
+
