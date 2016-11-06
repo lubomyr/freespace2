a959fcf403400f5c5d933fba66a41d8e96b02811
lubomyr <lubomyr31@gmail.com>
2016-11-06 16:53:59 +0200

    glshim library renamed to gl4es

--- AndroidBuild.sh
+++ AndroidBuild.sh
@@ -5,7 +5,7 @@ LOCAL_PATH=`cd $LOCAL_PATH && pwd`
 
 JOBS=1
 
-ln -sf libglshim.a $LOCAL_PATH/../../../obj/local/$1/libGL.a
+ln -sf libgl4es.a $LOCAL_PATH/../../../obj/local/$1/libGL.a
 
 env CFLAGS="-include ../../android_debug.h" \
 ../setEnvironment-$1.sh sh -c "cd freespace2 && make -j$JOBS" && mv -f freespace2/freespace2 libapplication-$1.so
